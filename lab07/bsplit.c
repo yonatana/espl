564375bbbb478bb122de9b612c6bc0b9ebac3c6a
yonatana <yonatana@post.bgu.ac.il>
2012-12-19 18:39:35 +0200

    niceday

---
+++ lab07/bsplit.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "xsum.h"
+void newFile(char* newName,char* name, int fileCount);
+
+
+int main(int argc, char** argv){
+
+  int CALL_CHKSUM=0;
+  int CHUNK_SIZE=1024;
+  int fileCount=0;
+  int isFinished=1;
+  unsigned int fileChecksum = 0;
+  char* fileName;
+  int opt;
+  
+  while((opt = getopt(argc, argv,"-hx:b:"))!=-1){
+    switch (opt) {
+      case 1:
+	fileName = optarg;
+	break;
+      case 'h':
+	printf("OPTIONS\n\t-h\t\tprint a summary of options and exit\n\t-b SIZE\t\tput at most SIZE bytes per output file\n\t-x\t\tprint the checksum as a hexadecimal rather than decimal number.\n");
+	exit(0);
+      case 'x':
+	CALL_CHKSUM=1;
+	fileName = argv[argc-1];
+	break;
+      case 'b':
+	if(optarg!=0) CHUNK_SIZE = atoi(optarg);
+	fileName = argv[argc-1];
+	break;  
+      default:
+	fprintf(stderr,"Usage: %s [-h] [-x] [-b SIZE] FILE\n",argv[0]);
+	exit(1);
+    }
+  } 
+  FILE *fp = fopen(fileName, "r");
+  if(fp==0){
+    printf("The filename argument you provided is not valid\n");
+    return 0;
+  }
+  unsigned char chunk[CHUNK_SIZE];
+  memset(chunk,0,CHUNK_SIZE);
+  unsigned char checksum = 0;
+  int fileSize = 0;
+  struct stat st = {0};
+  if (stat("parts", &st) == -1) {
+    mkdir("parts", 0700);
+  }
+ 
+  while (isFinished){
+    if ((fileSize = fread(&chunk, 1, CHUNK_SIZE, fp))==0) break;
+    unsigned int chunksum=0;
+    char newFileName[strlen(fileName)+15];  //create new file name
+    fileCount++;
+    newFile(newFileName,fileName,fileCount);
+    FILE* file = fopen(newFileName,"w+"); //new file
+    fwrite(&chunksum,sizeof(chunksum),1,file);	//write 0000 at the beginning
+    fwrite(&chunk,fileSize,1,file);
+    chunksum = chksum(file);//do chksum
+    fileChecksum^=chunksum;//overall chanksum
+    rewind(file);//look at first byte and write the new chanksum instand of 0000
+    fwrite(&chunksum,sizeof(chunksum),1,file);//write
+    memset(chunk,0,CHUNK_SIZE);//write 0 to chanksum (clean)
+    if (fileSize<CHUNK_SIZE){//is theis dfinal chank
+      isFinished=0;
+      fclose(file);
+    }
+  }
+
+
+  if (CALL_CHKSUM==1){//return as hex
+    if (fileChecksum !=0){
+      printf("Checksum: %u\n",fileChecksum);
+    }
+  }
+
+return 0;
+}
+
+void newFile(char* newName,char* name, int fileCount){
+  char num[5] = {0};
+  sprintf(num,"%d",fileCount);
+  newName[0] = 0;
+  strcat(newName,"parts/");
+  strcat(newName,name);
+  strcat(newName,".");
+  if(fileCount<10) strcat(newName,"0");
+  strcat(newName,num);
+}
