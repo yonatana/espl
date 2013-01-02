d37d09d90af68d18a2e1945742ed4f5968dcfc94
yonatana <yonatana@post.bgu.ac.il>
2013-01-02 16:17:46 +0200

    phyton

--- reverse.py
+++ reverse.py
@@ -1,6 +1,6 @@
 import sys
-for arg in reversed(sys.argv):
-	pring arg 
+for arg in reversed(sys.argv):#pass on args in reverced use the sys.method taking arg
+	print arg, 
 	
-raw_input(press<enter>) 
+#raw_input(press<enter>) 
 	
